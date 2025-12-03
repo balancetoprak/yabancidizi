5915bea738b5a75f72f03f8cf57ca0c7dff1d4f7
misu <mishucon@pm.me>
2025-12-04 02:38:01 +0300

    Update hero.ts

--- src/utils/hero.ts
+++ src/utils/hero.ts
@@ -4,14 +4,12 @@ export default heroui({
   themes: {
     light: {
       colors: {
-        // @ts-expect-error
         "secondary-background": "#F4F4F5",
       },
     },
     dark: {
       colors: {
         background: "#0f0f0f",
-        // @ts-expect-error
         "secondary-background": "#18181B",
       },
     },
